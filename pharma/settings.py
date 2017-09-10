4e72c8f57f756b89cc45463d69516b8c695c8ae9
saghirhussain <saghirmhussain@gmail.com>
2017-09-10 13:01:02 +0100

    Update settings.py

--- pharma/settings.py
+++ pharma/settings.py
@@ -20,10 +20,15 @@ MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6ett%fiyk^vxmedaz1=1y^q=&bk_gy*qr6@9^s$chxe%8!=9pv'
+# SECRET_KEY = '6ett%fiyk^vxmedaz1=1y^q=&bk_gy*qr6@9^s$chxe%8!=9pv'
+
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['127.0.0.1', 'saghirhussain.pythonanywhere.com']
 
@@ -44,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,11 +124,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+
+
 
 #you need to use the below code without static_root if you want to serve bootstrap locally
 STATICFILES_DIRS = [
@@ -145,3 +153,12 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'williamhowl98@gmail.com'
 EMAIL_HOST_PASSWORD = 'Dumbledor1$'
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 6000
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
