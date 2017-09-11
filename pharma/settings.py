51404dd8d924506be931a302c743a13f5f0db67b
saghirhussain <saghirmhussain@gmail.com>
2017-09-11 18:53:45 +0100

    setting for S3

--- pharma/settings.py
+++ pharma/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://evening-shelf-11870.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'evening-shelf-11870.herokuapp.com']
 
 
 # Application definition
