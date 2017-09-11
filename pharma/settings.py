6d8aba325f66bb432c35846d22432610cbf7a486
saghirhussain <saghirmhussain@gmail.com>
2017-09-11 15:36:19 +0100

    setting for S3

--- pharma/settings.py
+++ pharma/settings.py
@@ -161,11 +161,12 @@ DATABASES['default'].update(db_from_env)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # if need change to s3 storage then add 'pharma/' before static
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'), 
+    os.path.join(BASE_DIR, 'pharma/static'),
 ]
 
 AWS_ACCESS_KEY_ID = 'AKIAI33LHDZ43QNETJUA'
@@ -178,7 +179,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 AWS_LOCATION = 'static'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 DEFAULT_FILE_STORAGE = 'pharma.storage_backends.MediaStorage'  
